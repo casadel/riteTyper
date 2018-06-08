8cc39b97f08f7c132d5d4c26d1b2fc524f5afdd9
Alister Bent <abent@college.harvard.edu>
2018-06-08 12:51:09 -0400

    fix sector choose

--- randomHeads.py
+++ randomHeads.py
@@ -43,11 +43,8 @@ for row, index in exc_list_mstr.iterrows():
     sectors = ['Technology', 'Health Care', 'Consumer Services', 'Consumer Non-Durables', 'Miscellaneous', 'Consumer Durables', 'Basic Industries', 'Capital Goods', 'Transportation', 'Energy', 'Finance', 'Public Utilities']
     
     ####Choose sector#######
-    inpt = raw_input('Choose Sector!!! (0-Tech, 1-Hlth, 2-Services, 3-Consumer, 4-Misc, 5-Con. Durables, 6-Basic Inds., 7-Cap. Goods, 8-Trans., 9-Energy, 10-Fin., 11-XLU)')
-    while isinstance(inpt, int)==False:
-        print 'Please use integers 0-12'
-        inpt = raw_input('Choose Sector!!! (0-Tech, 1-Hlth, 2-Services, 3-Consumer, 4-Misc, 5-Con. Durables, 6-Basic Inds., 7-Cap. Goods, 8-Trans., 9-Energy, 10-Fin., 11-XLU)')
-    sector_str = sectors[inpt]
+    inpt = raw_input('Choose Sector!!! (0-Tech, 1-Hlth, 2-Services, 3-Consumer, 4-Misc, 5-Con. Durables, 6-Basic Inds., 7-Cap. Goods, 8-Trans., 9-Energy, 10-Fin., 11-XLU)\n
+    sector_str = sectors[int(inpt)]
     condition = (industry != 'Precious Metals' and industry != 'Real Estate Investment Trusts' and industry != 'Marine Transportation')
     if carat == False and letters == True and sector == sector_str and condition: 
         if market_cap > 750000000 and market_cap < 35000000000:
